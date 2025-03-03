2428da4eae2d8413f0a4f3b6168c5496ef84855a
Volodymyr Tabashniuk <60541362+vtabashniuk@users.noreply.github.com>
2025-03-03 10:45:57 +0200

    change token expiration time

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const generateToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "4h" });
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 // Реєстрація користувача
